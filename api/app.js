dfa8f11e68ff485ff62e9835b8fa6b49c4ca0a5e
Fernanda Amorim <fernanda.amorim@CLBBRC02ZXJV6MD6M.local>
2023-07-28 22:59:14 -0300

    readme

--- api/app.js
+++ api/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Porta em que o servidor irá rodar
+const port = 8080; // Porta em que o servidor irá rodar
 
 // Endpoint que responde com um JSON
 app.get('/', (req, res) => {
-  const data = {
-    message: 'Esse é o Meu trabalho de Node.js!! Fernanda Amorim',
-    date: new Date()
-  };
-  res.json(data);
+  res.send('Esse é o Meu trabalho de Node.js!! Fernanda Amorim')
 });
 
-// Inicie o servidor
+// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
