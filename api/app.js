33e3a2067e93cfe33869173eb4deaaa7805c4c93
Fernanda Amorim <fernanda.amorim@CLBBRC02ZXJV6MD6M.local>
2023-08-01 21:47:37 -0300

    mudancas

--- api/app.js
+++ api/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 8080; // Porta em que o servidor irá rodar
+const rateLimit = require('express-rate-limit');
+
+//Limitar quantidade de requisiçoes 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5, 
+  message: 'Muitas requisições, Tente mais tarde!'
+});
+
+app.use(limiter);
 
 // Endpoint que responde com um JSON
 app.get('/', (req, res) => {
-  res.send('Esse é o Meu trabalho de Node.js!! Fernanda Amorim')
+  const data = {
+  Nome: 'Fernanda Amorim',
+  Projeto: 'Trabalho Node.Js- Campinho/2023',
+  date: new Date(),
+  }
+  res.json(data);
 });
 
 // Iniciar o servidor
